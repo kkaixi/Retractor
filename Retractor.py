bfa4de258fdf3acaac72f508eb02fc189f99b675
kkaixi <35850858+kkaixi@users.noreply.github.com>
2019-04-04 17:13:41 -0400

    2019-04-04

--- Retractor.py
+++ Retractor.py
@@ -4,25 +4,38 @@ Created on Fri Jun  1 10:30:50 2018
 
 @author: tangk
 """
-from PMG.COM.data import import_data
 import pandas as pd
-from PMG.COM import arrange
-directory = 'C:\\Users\\tangk\\Desktop\\Retractor Data\\'
-channels = ['12CHST0000Y7ACXC',
-            '12CHST0000Y7DSXB',
-            '12PELV0000Y7ACXA']
-cutoff = range(100,1600)
-t, fulldata = import_data(directory,channels)
-chdata = arrange.test_ch_from_chdict(fulldata,cutoff)
-t = t.get_values()[cutoff]
-se = list(chdata.index)
+import numpy as np
+from PMG.read_data import initialize
+from PMG.COM.arrange import *
+from PMG.COM.plotfuns import *
 
-for ch in channels:
-    for s in se:
-        if s=='SE18-0053_21' or s=='SE18-0053_22':
-            plt.plot(t,chdata[ch][s],'b',label='3.75')
-        else:
-            plt.plot(t,chdata[ch][s],'r',label='4.75')
-    plt.title(ch)
-    plt.legend()
-    plt.show()
+directory = 'P:\\Data Analysis\\Projects\\Retractor\\'
+cutoff = range(100,1600)
+channels = ['12CHST0000Y7ACXC',
+            '14CHST0000Y7ACXC']
+drop = ['SE17-0039_3']
+
+table, t, chdata = initialize(directory,channels, cutoff, drop=drop)
+
+#%% Compare of in-vehicle vs. bench + original buckle + vehicle retractor vs. bench + flexible buckle + vehicle retractor 
+plot_channels = [['12CHST0000Y7ACXC','14CHST0000Y7ACXC']]
+#subset = (table.query('Model==\'Turbo Booster\'')
+#               .table.query_list('Buckle',['Original','Short Flex','Vehicle'])
+#               .table.query_list('Retractor', ['Marc_4','None','Vehicle']))
+
+
+subset = (table.query('Model==\'Harmony\'')
+               .table.query_list('Buckle',['Short Flex'])
+               .table.query_list('Retractor',['Vehicle','Marc_4'])) 
+
+
+subset['Cond'] = subset[['Retractor','Buckle']].apply(lambda x: '+'.join(x), axis=1)
+grouped = subset.groupby('Cond')
+for grp in grouped:
+    for ch in plot_channels:
+        x = {'a': merge_columns(chdata.loc[grp[1].index, ch])}
+        fig, ax = plt.subplots()
+        ax = plot_overlay(ax, t, x)
+        ax = set_labels(ax, {'title': grp[0]})
+        ax.set_ylim([-70, 10])
